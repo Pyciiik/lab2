475b174604a963f3aed27f3350387ca35485a5b7
Pyciiik <140558619+Pyciiik@users.noreply.github.com>
2024-02-13 11:45:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title><?= 'My Site!' ?></title>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name="description" content="">
+ <meta name="author" content="">
+ <link rel="stylesheet" href="assets/css/my-styles.css">
+ <link rel="stylesheet" href="assets/Stylesheet/my-styles.css">
+</head>
+<body>
+<header id="header" class="header">
+    <div class="container">
+        <p class="story">Story</p>
+        <div class="top-bar">
+            <div class="block">
+                <p class="marcy">Marcy..</p>
+            </div>
+        </div>
+        <div class="nav-menu">
+            <ul class="menu">
+                <li class="link">Home Page</li>
+                <li class="link">About Us</li>
+                <li class="link">Contact Us</li>
+            </ul>
+        </div>
+    </div>
+</header>
+<main class="main content">
+</main>
+<footer class="footer">
+ <div class="container text-center">
+ <small class="copyright"></small>
+ </div>
+</footer>
+</body>
+</html>
